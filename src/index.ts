56d9cbd5a86ec61862e171b143052c4b5e8897f4
Ashary Vermaysha <asharyver13@gmail.com>
2025-09-13 14:15:08 +0700

    feat(server): :sparkles: update server initialization with hostname and port configuration
    * Added hostname and port configuration to enhance server setup
    * Ensured server listens on specified port and hostname for better accessibility

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import { runMigration, sql } from "./database";
+import { runMigration, sql } from './database';
+import { server } from './server';
 
 if (sql.options.adapter !== 'postgres') {
   throw new Error('Only postgres is supported');
 }
 
+const hostname = process.env.HOSTNAME || '0.0.0.0';
+const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+
 const main = async () => {
   await runMigration();
-}
+  server.listen({ port, hostname }, (app) => {
+    console.log(`Server running at http://${app.hostname}:${app.port}`);
+  });
+};
 
 main();
