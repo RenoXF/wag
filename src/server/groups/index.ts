f6b23f23115e2a86f018d0e0ae2f0608acfa70bf
Ashary Vermaysha <asharyver13@gmail.com>
2025-09-15 08:36:45 +0700

    refactor(groups): :recycle: improve code formatting and structure
    * Enhanced readability by adjusting whitespace and formatting
    * Organized the code for better maintainability

--- src/server/groups/index.ts
+++ src/server/groups/index.ts
@@ -1,16 +1,49 @@
-import {Elysia} from 'elysia';
+import { Elysia } from 'elysia';
 import { Group } from './service';
 import { GroupModel } from './model';
+import { WaStore } from '../../whatsapp';
 
 export const groups = new Elysia({
-  prefix: '/groups'
+  prefix: '/groups',
 })
-.get('/', async ({query}) => {
-  const data = await Group.getAll({deviceId: query.deviceId});
+  .get(
+    '/',
+    async ({ query }) => {
+      const data = await Group.getAll({ deviceId: query.deviceId });
 
-  return {
-    data,
-  };
-}, {
-  query: GroupModel.index
-})
+      return {
+        data,
+      };
+    },
+    {
+      query: GroupModel.index,
+    }
+  )
+  .post(
+    '/refresh',
+    async ({ body, set }) => {
+      const whatsapp = WaStore.get(body.deviceId);
+
+      if (!whatsapp) {
+        set.status = 404;
+        return {
+          error: 'Device not found',
+        };
+      }
+
+      const res = await whatsapp.refreshGroupMetadata();
+
+      if (res) {
+        return { success: true };
+      } else {
+        set.status = 500;
+
+        return {
+          error: 'Failed to refresh group metadata, please try again later',
+        };
+      }
+    },
+    {
+      body: GroupModel.index,
+    }
+  );
