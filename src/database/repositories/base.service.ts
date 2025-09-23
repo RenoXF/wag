6b2f82645ca90a3ecec5870cf7bca5ccf46679e0
Ashary Vermaysha <asharyver13@gmail.com>
2025-09-23 15:06:02 +0700

    feat(devices): :sparkles: add devices repository to base database service
    * Introduced the devices repository to the BaseDatabaseService class.
    * This change allows for better management and interaction with device-related data.

--- src/database/repositories/base.service.ts
+++ src/database/repositories/base.service.ts
@@ -1,9 +1,10 @@
 import type { SQL, TransactionSQL } from "bun";
-import type { IContactsRepository, IDatabaseService, IGroupsRepository, IMessagesRepository, ISessionsRepository } from "./interfaces";
+import type { IContactsRepository, IDatabaseService, IDevicesRepository, IGroupsRepository, IMessagesRepository, ISessionsRepository } from "./interfaces";
 
 export abstract class BaseDatabaseService implements IDatabaseService {
 
   abstract contacts: IContactsRepository;
+  abstract devices: IDevicesRepository;
   abstract groups: IGroupsRepository;
   abstract messages: IMessagesRepository;
   abstract sessions: ISessionsRepository;
