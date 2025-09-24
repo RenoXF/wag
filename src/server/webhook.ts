8b4dc1184e1d4a994f6f8c31e6f8fac7bec32693
Ashary Vermaysha <asharyver13@gmail.com>
2025-09-24 09:06:18 +0700

    refactor(webhook): :recycle: enhance error handling with Sentry tracing
    * Added Sentry tracing to capture errors when sending webhooks
    * Improved error logging for better debugging

--- src/server/webhook.ts
+++ src/server/webhook.ts
@@ -1,3 +1,5 @@
+import { traceSentry } from "@/instrument";
+
 export const sendWebhook = async   (
   data: object,
   webhookUrl?: string | null,
@@ -9,6 +11,12 @@ export const sendWebhook = async   (
       body: JSON.stringify(data),
       signal: AbortSignal.timeout(30_000) // 30 seconds timeout
     }).catch((err) => {
+      traceSentry(err, {
+        data: {
+          payload: data,
+          webhookUrl,
+        }
+      });
       console.error('Error sending auth webhook:', err?.message);
     });
   }
