723e2cff9766b97eae8a476c5be844211fc6c407
Ashary Vermaysha <asharyver13@gmail.com>
2026-01-20 13:21:25 +0700

    feat: add OpenAPI documentation and new HTML dashboard for WhatsApp Gateway API

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,33 @@
-import indexClient from '@/index.html';
+import indexClient from './index.html';
 import { logger } from '@/logger';
 import { Elysia } from 'elysia';
 import { connections } from './connections';
 import { messages } from './messages';
+import { openapi } from '@elysiajs/openapi'
+import { version } from 'package.json';
 
 const app = new Elysia()
+  .use(openapi({
+    path: '/docs',
+    documentation: {
+      info: {
+        title: 'WhatsApp Gateway API',
+        description: 'An API to interact with WhatsApp accounts programmatically.',
+        version: version,
+        license: {
+          name: 'MIT',
+          url: 'https://opensource.org/license/mit/',
+        },
+      }
+    }
+  }))
   .use(connections)
   .use(messages)
-  .get('/', indexClient)
+  .get('/', indexClient, {
+    detail: {
+      hide: true,
+    }
+  })
   .listen(
     {
       port: Bun.env.PORT ? Number(Bun.env.PORT) : 3000,
