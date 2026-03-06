e6c5f302144f50121d621075c05d946964e16c83
Ashary Vermaysha <asharyver13@gmail.com>
2026-03-06 13:48:14 +0700

    feat: add CSS and ICO module declarations, update icon paths in HTML and server index

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,13 @@ import { logger } from '@/logger';
 import { openapi } from '@elysiajs/openapi';
 import { Elysia } from 'elysia';
 import { version } from 'package.json';
-import indexClient from './client/index.html';
+import indexClient from './client/index.html' with { type: 'text' };
+import clientCss from './client/client.css' with { type: 'text' };
+import icon from '../../assets/icon.ico' with { type: 'file' };
 import { connections } from './connections';
 import { logs } from './logs';
 import { messages } from './messages';
+import { file } from 'bun';
 
 const app = new Elysia()
   .use(
@@ -28,11 +31,25 @@ const app = new Elysia()
   .use(connections)
   .use(messages)
   .use(logs)
-  .get('/', indexClient, {
+  .get('/', ({set}) => {
+    set.headers['content-type'] = 'text/html';
+
+    return indexClient;
+  }, {
     detail: {
       hide: true,
     },
   })
+  .get('/client.css', ({set}) => {
+    set.headers['content-type'] = 'text/css';
+
+    return clientCss;
+  }, { detail: { hide: true } })
+  .get('/icon.ico', ({set}) => {
+    set.headers['content-type'] = 'image/x-icon';
+
+    return file(icon);
+  }, { detail: { hide: true } })
   .listen(
     {
       port: Bun.env.PORT ? Number(Bun.env.PORT) : 3000,
